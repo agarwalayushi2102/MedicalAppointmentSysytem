40d3a93b1f04f3ed8095f11827e8770f2dcd9e00
Ayushi Agarwal <95530356+agarwalayushi2102@users.noreply.github.com>
2022-08-10 19:31:01 +0530

    server-routes

--- index.js
+++ index.js
@@ -1,46 +1,90 @@
-$("#add_user").submit(function(event){
-    alert("Data Inserted Successfully!");
-})
+var express = require('express');
+var router = express.Router();
+const services = require('../services/render');
+var Userdb = require('../model/model');
 
-$("#update_user").submit(function(event){
-    event.preventDefault();
+router.get('/appointments', (req,res) => {
+  // validate request
+  if(req.query.id){
+    const id = req.query.id;
 
-    var unindexed_array = $(this).serializeArray();
-    var data = {}
+    Userdb.findById(id)
+        .then(data =>{
+            if(!data){
+                res.status(404).send({ message : "Not found user with id "+ id})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({ message: "Error retrieving user with id " + id})
+        })
 
-    $.map(unindexed_array, function(n, i){
-        data[n['name']] = n['value']
-    })
-
-
-    var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
-        "method" : "PUT",
-        "data" : data
-    }
-
-    $.ajax(request).done(function(response){
-        alert("Data Updated Successfully!");
-    })
-
-})
-
-if(window.location.pathname == "/"){
-    $ondelete = $(".table tbody td a.delete");
-    $ondelete.click(function(){
-        var id = $(this).attr("data-id")
-
-        var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
-            "method" : "DELETE"
-        }
-
-        if(confirm("Do you really want to delete this record?")){
-            $.ajax(request).done(function(response){
-                alert("Data Deleted Successfully!");
-                location.reload();
-            })
-        }
-
-    })
+}else{
+    Userdb.find()
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+        })
 }
+});
+
+
+
+router.post('/appointments', (req, res, next) => {
+  const { date, name, email } = req.body;
+   // new user
+   const user = new Userdb({
+    name : req.body.name,
+    email : req.body.email,
+    date: req.body.date,
+});
+  
+  if (!name){
+    return res.status(400).json({
+      message: 'Name are required',
+    });
+  }
+  if (!date){
+    return res.status(400).json({
+      message: 'Appointment Date are required',
+    });
+  }
+  if (!email){
+    return res.status(400).json({
+      message: 'Email are required',
+    });
+  }
+ 
+
+// save user in the database
+user
+  .save(user)
+  .then(result => res.json(result.ops[0]))
+  .catch(error => res.send(error))
+
+});
+
+router.delete('/appointments/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message : "User was deleted successfully!"
+                })
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message: "Could not delete User with id=" + id
+            });
+        });
+});
+
+module.exports = router;
